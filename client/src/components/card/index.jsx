9c33561a41acd70db253b63013fad8bbabc57e50
HAMZA NAWAB <hamzanawab.bs@gmail.com>
2022-10-23 14:53:12 +0500

    Sidebar almost completed

--- client/src/components/card/index.jsx
+++ client/src/components/card/index.jsx
@@ -3,9 +3,10 @@ import { FiMoreHorizontal } from "react-icons/fi";
 
 const Card = (props) => {
     return (
-        <div className={`flex ${props.isActive ? "bg-text-box" : null} p-4 rounded-lg w-full`}>
+        <div className={`flex ${props.unseen ? "bg-text-box" : null} p-4 rounded-lg w-full`}>
             <div className="bg-text-primary p-4 rounded-full relative mr-3">
                 <FaUserAlt className="text-text-box scale-110" />
+                {props.unseen && <div className="bg-secondary w-5 h-5 rounded-full absolute top-0 left-0 text-xs text-white flex items-center justify-center"><p>{props.unseen}</p></div>}
                 {props.ping && (<>
                     <div className="bg-green-500 w-3 h-3 rounded-full absolute right-0 animate-ping"></div>
                     <div className="bg-green-500 w-3 h-3 rounded-full absolute right-0"></div>
@@ -14,7 +15,7 @@ const Card = (props) => {
             <div className="w-full flex justify-between">
                 <div className="w-1/2 flex flex-col justify-between items-start">
                     <p className="text-text-normal">{props.name}</p>
-                    <p className="text-text-primary truncate text-sm">{props.message}</p>
+                    <p className={` truncate text-sm ${props.unseen ? "font-bold text-text-normal" : "text-text-primary"}`}>{props.message}</p>
                 </div>
                 <div className="flex flex-col justify-between items-end">
                     <button>
--- client/src/container/sider-bar.jsx
+++ client/src/container/sider-bar.jsx
@@ -3,11 +3,17 @@ import Input from '../components/input'
 import { FiSearch } from "react-icons/fi"
 import Button from '../components/button'
 import Card from '../components/card'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 const SideBar = () => {
     const [filter, setFilter] = useState(0);
-    
+    const lineBorder = useRef();
+    const changeFilterHandler = (filter) => {
+        setFilter(filter);
+        if (filter === 0) lineBorder.current.style.left = "2rem"; 
+        if (filter === 1) lineBorder.current.style.left = "8.5rem"; 
+        if (filter === 2) lineBorder.current.style.left = "15.25rem"; 
+    };
 
     return (
         <div className="w-1/4 h-full bg-background">
@@ -19,24 +25,47 @@ const SideBar = () => {
                 </div>
                 <Button shape="circle" text="+" className="absolute right-5 -bottom-3 font-bold" />
             </div>
-            <div className="w-full flex items-center justify-center flex-col relative">
+            <div className="w-full flex items-center justify-center flex-col relative mb-5">
                 <div className="w-11/12 h-16 flex items-center justify-around">
-                    <div className="text-secondary">Recent</div>
-                    <div className="text-text-primary hover:text-secondary cursor-pointer">Unread</div>
-                    <div className="text-text-primary hover:text-secondary cursor-pointer">Groups</div>
-                    <div className="bg-secondary w-16 h-1 absolute left-50 bottom-1 rounded-lg"></div> 
-                    {/* 8  50 */}
-                    {/* right 8 */}
+                    <div className={`cursor-pointer ${filter === 0 ? "text-secondary" : "text-text-primary hover:text-secondary"}`} onClick={() => changeFilterHandler(0)}>Recent</div>
+                    <div className={`${filter === 1 ? "text-secondary" : "text-text-primary hover:text-secondary"} cursor-pointer`} onClick={() => changeFilterHandler(1)}>Unread</div>
+                    <div className={`${filter === 2 ? "text-secondary" : "text-text-primary hover:text-secondary"} cursor-pointer`} onClick={() => changeFilterHandler(2)}>Groups</div>
+                    <div className="bg-secondary w-16 h-1 absolute bottom-1 rounded-lg transition-all ease-in-out" style={{ left: "15.25rem" }} ref={lineBorder}></div>
                 </div>
                 <hr className='bg-text-box w-11/12 absolute bottom-1' />
             </div>
-            {/* <div className="overflow-y-auto">
-                <div className="flex items-center justify-center">
-                    <div className="w-11/12">
-                        <Card isActive={true} name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+            <div className="h-96">
+                <div className="flex items-center justify-center h-full">
+                    <div className="w-11/12 overflow-y-auto h-full">
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
+                        <Card name="Hamza" message="Hello world" time="7:39 am" ping={true} />
                     </div>
                 </div>
-            </div> */}
+            </div>
         </div>
     )
 }
