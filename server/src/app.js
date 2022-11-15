d93a797e32b61a06a233aec5b26359f1c8cd328b
usmanGhani16 <afro123737@gmail.com>
2022-11-16 01:09:55 +0500

    basic server setup

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+module.exports = app;
---
+++ server/src/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const app = require('./app');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
+const server = http.createServer(app);
+
+function startServer() {
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+startServer();
